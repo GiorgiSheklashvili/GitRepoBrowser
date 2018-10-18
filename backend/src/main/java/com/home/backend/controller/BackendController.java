5d55063170b40bd72e1a0a569fd381b15167e3ba
Giorgi Sheklashvili <sheklashviligiorgi@gmail.com>
2018-10-19 01:07:50 +0300

    first commit

---
+++ backend/src/main/java/com/home/backend/controller/BackendController.java
@@ -0,0 +1,26 @@
+package com.home.backend.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@RestController()
+@RequestMapping("/api")
+public class BackendController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(BackendController.class);
+
+    public static final String HELLO_TEXT = "Hello from Spring Boot Backend!";
+
+    @RequestMapping(path = "/hello")
+    public @ResponseBody String sayHello() {
+        LOG.info("GET called on /hello resource");
+        return HELLO_TEXT;
+    }
+}
